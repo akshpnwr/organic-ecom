3e48c1b70163c37155f2b599b84549c9a06b05ac
akshpnwr <akashpanwar9976@gmail.com>
2024-03-20 17:35:04 +0530

    Added top category component

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import FeaturedProducts from "./components/featured-products/featured-products";
 import Hero from "./components/hero/hero";
 import Navigation from "./components/navigation/navigation";
 import Testimonial from "./components/testimonial";
-import banner from "./assets/images/featured-products-banner.png";
+import banner1 from "./assets/images/homepage-ad-banner-1.png";
+import banner2 from "./assets/images/homepage-ad-banner-2.png";
+import banner3 from "./assets/images/homepage-ad-banner-3.png";
+import TopCategory from "./components/top-category";
+import Footer from "./components/footer";
 
 function App() {
   return (
@@ -10,13 +14,14 @@ function App() {
       <Navigation />
       <Hero />
       <FeaturedProducts />
-
-      <div className="px-4 md:px-0 py-10 flex flex-col md:flex-row justify-center gap-x-20">
-        <img src={banner} alt="" />
-        <img src={banner} alt="" />
-        <img src={banner} alt="" />
+      <TopCategory />
+      <div className="px-4 my-0 mx-auto py-10 md:px-0 flex flex-col justify-center md:flex-row gap-10 ">
+        <img className="w-[23%] hover:scale-105" src={banner1} alt="" />
+        <img className="w-[23%] hover:scale-105" src={banner2} alt="" />
+        <img className="w-[23%] hover:scale-105" src={banner3} alt="" />
       </div>
       <Testimonial />
+      <Footer />
     </div>
   );
 }
